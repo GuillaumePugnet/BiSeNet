e620a4beebfb84cf028099c992e878cd3250210f
GuillaumePugnet <guillaume.pugnet@outlook.com>
2022-11-10 23:11:07 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='bisenet',
+    version='1.0.0',    
+    description='BiSeNet',
+    url='https://github.com/GuillaumePugnet/BiSeNet',
+    author='',
+    author_email='',
+    license='',
+    packages=['bisenet'],
+    install_requires=[                   
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',   
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
